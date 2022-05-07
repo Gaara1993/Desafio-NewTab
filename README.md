b8eeddf2c4b80967c45797d20408405810c071d7
MatheusOCSXP <matheus.ocs@gmail.com>
2022-05-07 19:43:20 -0300

    Limpeza do código e adição do README

---
+++ README.md
@@ -0,0 +1,19 @@
+Desafio NewTab Academy
+
+Para demonstrar que você tem um conhecimento inicial ou consegue se dedicar para aprender rapidamente um conteúdo básico, temos o seguinte projeto para você: um formulário simples de soma de dois valores com HTML e JavaScript.
+
+Crie uma página HTML com os seguintes campos:
+
+Valor 1 (input text)
+Valor 2 (input text)
+Somar (input button)
+Resultado (input text)
+
+Ao clicar no botão "Somar", você deve recuperar o "Valor 1" preenchido e somar com o "Valor 2", utilizando uma função JavaScript. O resultado dessa somatória deve ser exibido no campo "Resultado".
+
+Além da operação citada acima, você deve validar os seguintes valores:
+
+Campos Valor 1 e Valor 2 não podem estar vazios quando clicar em Somar.
+Campos Valor 1 e Valor 2 não podem ser menores que zero quando clicar em Somar.
+
+https://matheusocsxp.github.io/Desafio-NewTab/
--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-/*
-function somar() {
-    var tn1 = window.document.getElementById('txtn1');
-    var tn2 = window.document.getElementById('txtn2');
-
-    if (tn1 !== "" && tn2 !== "") {
-
-        var n1 = Number(tn1.value);
-        var n2 = Number(tn2.value);
-
-        if (n1 < 0 || n2 < 0 || n1 === "" || n2 === "") {
-            window.alert('Não são permitidos valores vazios ou negativos nos campos Valor 1 e Valor 2');
-        } else {
-            res.value = n1 + n2;
-        }
-
-    } else {
-        window.alert('Por favor, digite um número!');
-    }
-
-
-}
-*/
-
 function somar() {
   var tn1 = window.document.getElementById("txtn1");
   var tn2 = window.document.getElementById("txtn2");
@@ -43,36 +19,3 @@ function somar() {
     res.value = n1 + n2;
   }
 }
-
-/*
-    var btnSum = document.getElementById('btnSum');
-    btnSum.addEventListener('click', somar);
-    
-    function somar() {
-        const firstValue = document.getElementById('txtn1').value;
-        const secondValue = document.getElementById('txtn2').value;
-        const res = document.getElementById('res');
-    
-    
-        const isNull = firstValue === "" || secondValue === "";
-        const isNotNumber = Number.isNaN(Number(firstValue)) || Number.isNaN(Number(secondValue))
-        const lessThanZero = firstValue < 0 || secondValue < 0;
-    
-        if (isNull) {
-            alert("Preencha todos os campos!");
-            res.value = ""
-        } else if (isNotNumber) {
-            alert('Digite um número válido!');
-            res.value = ""
-        } else if (lessThanZero) {
-            alert('Digite um valor Maior que zero!');
-            res.value = ""
-        } else {
-            const firstValueNumber = Number(firstValue)
-            const secondValueNumber = Number(secondValue)
-    
-    
-            res.value = firstValueNumber + secondValueNumber
-        }
-    }
-    */
